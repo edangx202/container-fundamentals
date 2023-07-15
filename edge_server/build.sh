44c61da0cdcc262bd070aa347b886c502cefe666
edangx202 <129401428+edangx202@users.noreply.github.com>
2023-07-15 14:30:41 +0000

    week 1 update

---
+++ edge_server/build.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/env bash
+
+# docker build -t edge_server:v1 . 
+docker build -t edge_server_small:v1 . 
---
+++ edge_server/deploy.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/env bash
+
+# docker run -p 8000:80 -d edge_server:v1
+docker run -p 8000:80 -d edge_server_small:v1
